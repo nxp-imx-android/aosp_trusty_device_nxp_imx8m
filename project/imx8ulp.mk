2c0eea40cd6654aa492902293117f23f3bd97625
Ji Luo <ji.luo@nxp.com>
2021-06-24 16:19:49 +0800

    MA-19209 imx8ulp: add trusty support
    Add build target for imx8ulp.
    
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Change-Id: I514976b42702d8892e5f456a95bd8e925458655b

---
+++ project/imx8ulp.mk
@@ -0,0 +1,25 @@
+# Copyright (C) 2015 The Android Open Source Project
+# Copyright NXP 2021
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+TARGET := imx8ulp
+
+#Enlarge imx8ulp storage size to 2048 blocks.
+STORAGE_RPMB_BLOCK_COUNT = 2048
+MEMBASE           := 0x96000000
+IMX_USE_LPUART := true
+SMP_MAX_CPUS := 2
+
+include project/imx8-inc.mk
