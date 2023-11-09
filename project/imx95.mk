3e17fff1968031d086c6f24d4b2d1ee9cfb55b4d
Ji Luo <ji.luo@nxp.com>
2023-11-09 11:07:39 +0800

    MA-21906 add imx95 build target
    Add build target for i.MX 95.
    
    Change-Id: I7c11089fe8d96caee02f26910105ae10f7a483a6
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/23243
    Reviewed-by: Elven Wang <elven.wang@nxp.com>

---
+++ project/imx95.mk
@@ -0,0 +1,33 @@
+# Copyright (C) 2015 The Android Open Source Project
+# Copyright NXP 2023
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+TARGET := imx95
+
+#Enlarge imx95 storage size to 2048 blocks.
+STORAGE_RPMB_BLOCK_COUNT = 2048
+MEMBASE           := 0x8C000000
+IMX_USE_LPUART := true
+SMP_MAX_CPUS := 6
+
+# ELE support
+WITH_ELE_SUPPORT := true
+
+include project/imx8-inc.mk
+
+TRUSTY_BUILTIN_USER_TASKS += \
+    trusty/user/app/sample/hwaes
+
+GLOBAL_DEFINES += GIC600=1
