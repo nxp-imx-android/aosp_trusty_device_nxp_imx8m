873844164d910f38eba221152a00f12976c768f7
Jiri Rezler <jiri.rezler@nxp.com>
2024-10-25 05:55:00 +0200

    AAUTO-1385 NXP copyright format has been fixed.

--- project/imx8-inc.mk
+++ project/imx8-inc.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2018 The Android Open Source Project
-# Copyright NXP 2018
+# Copyright 2018 NXP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
